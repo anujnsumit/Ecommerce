39ffd71bc2e169c3d1ef31c95779e3689ec6f44b
Sumit.Kumar <sumit.kumar@qtsolv.com>
2023-04-09 16:23:16 +0530

    server folder for server side code

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import colors from 'colors';
+import dotenv from 'dotenv';
+import dbConnect from "./config/db.js";
+import router from "./routes/authRoute.js";
+
+// dotenv
+dotenv.config({path:'./config/config.env'});
+
+const app=express();
+
+// database connection
+dbConnect();
+
+// middleware
+app.use(express.json());
+// app.use(morgan('dev'))
+
+// Routes
+app.use('/api/v1/auth',router);
+
+const PORT=process.env.PORT || 8080;
+
+app.listen(PORT,()=>console.log(`sever is running at ${PORT} enviorment ${process.env.NODE_ENV}`.bgCyan.white))
